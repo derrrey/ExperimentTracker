a54d5f52a19f9cc2cf238954f70e1e68a026a44b
derrrey <rene.schroeder38@gmail.com>
2016-07-22 19:05:37 +0200

    basic setup

---
+++ ExperimentTracker/ExperimentTracker.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Reflection;
+using System.Linq;
+using System.Text;
+using KSP.UI.Screens;
+using KSP.IO;
+
+using UnityEngine;
+
+namespace ExperimentTracker
+{
+    /** Load in Flight scene once */
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+
+    public class ExperimentTracker : MonoBehaviour
+    {
+        /** modTag to use in every debug message */
+        private static string modTag = "[ExperimentTracker]: ";
+
+        /** Used variables */
+        private static ApplicationLauncherButton etButton;
+        private bool isActive;
+        private Texture2D onActive;
+        private Texture2D onInactive;
+
+        /** Called once at startup */
+        public void Awake()
+        {
+            debugPrint("Awake()");
+
+            /** Config loading setup */
+            PluginConfiguration config = PluginConfiguration.CreateForType<ExperimentTracker>();
+            config.load();
+            isActive = config.GetValue<bool>("isActive");
+
+            /** Load textures */
+            onActive = loadTexture("ExperimentTracker/icons/ET_active");
+            onInactive = loadTexture("ExperimentTracker/icons/ET_inactive");
+
+            /** Register for events */
+            GameEvents.onGUIApplicationLauncherReady.Add(setupButton);
+        }
+
+        /** Called on destroy */
+        public void OnDestroy()
+        {
+            debugPrint("OnDestroy()");
+
+            /** Save to config */
+            PluginConfiguration config = PluginConfiguration.CreateForType<ExperimentTracker>();
+            config.SetValue("isActive", isActive);
+            config.save();
+
+            /** Unregister for events */
+            GameEvents.onGUIApplicationLauncherReady.Remove(setupButton);
+        }
+
+        /** Set up for the toolbar-button */
+        public void setupButton()
+        {
+            if (ApplicationLauncher.Ready)
+            {
+                if (etButton == null)
+                {
+                    debugPrint("Setting up button");
+                    ApplicationLauncher instance = ApplicationLauncher.Instance;
+                    etButton = instance.AddModApplication(toggleActive, toggleActive, null, null, null, null, ApplicationLauncher.AppScenes.FLIGHT, getButtonTexture());
+                }
+            }
+        }
+
+        /** Get correct button texture */
+        private Texture2D getButtonTexture()
+        {
+            return isActive ? onActive : onInactive;
+        }
+
+        /** Called when button is pressed */
+        private void toggleActive()
+        {
+            debugPrint("toggleAction()");
+            isActive = !isActive;
+            etButton.SetTexture(getButtonTexture());
+        }
+
+        /** Load and return a texture */
+        private static Texture2D loadTexture(string path)
+        {
+            debugPrint("Loading (Texture): " + path);
+            return GameDatabase.Instance.GetTexture(path, false);
+        }
+
+        private static void debugPrint(string s)
+        {
+            print(modTag + s);
+        }
+    }
+}
