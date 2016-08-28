7d40c9183225937f44873a5acb54d92fd9f62f47
René Schröder <rschr@tzi.de>
2016-08-28 22:02:19 +0200

    adds INFO string to Text.cs

--- ExperimentTracker/Text.cs
+++ ExperimentTracker/Text.cs
@@ -10,5 +10,6 @@ namespace ExperimentTracker
         public const string MODTAG = "[ExperimentTracker]: ";
         public const string MODNAME = "ExperimentTracker";
         public const string NOTHING = "Nothing to do";
+        public const string INFO = "Info";
     }
 }
